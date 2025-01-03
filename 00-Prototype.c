2c07726fa2bbeb953bb46e130e24c6dc246e5d2d
Prodige Manakel OKO-ALPHOKO <prodigeoko19@gmail.com>
2025-01-03 00:37:36 +0000

    Create 00-Prototype.c

---
+++ 00-Prototype.c
@@ -0,0 +1,57 @@
+//Importation des bibliothèques
+
+//Déclaration des structures
+
+// Fonction pour allouer un nouveau client
+
+// Fonction pour allouer un nouveau compte
+
+// Fonction pour ajouter un client au début de la liste
+
+// Fonction pour ajouter un compte au début de la liste
+
+// Fonction pour copier les clients dans le fichier clients.txt
+
+// Fonction pour copier les comptes dans le fichier comptes.txt
+
+// Fonction pour enregistrer l'opération dans le fichier operations.txt
+
+// Fonction pour effectuer un dépôt
+
+// Fonction pour effectuer un retrait
+
+int main(){
+    // Initialisation de la tête du client
+
+    // Initialisation de la tête du compte
+
+    // Variable pour le choix de l'utilisateur
+
+    //Début du programme
+
+        // 1 Ajout d'un client
+
+            //copie des clients en tete de liste
+
+        //2 Ajout d'un compte
+
+            //copie des comptes en tete de liste
+
+        //3 Operation sur un compte
+
+            //Depot sur le compte
+                
+                //Ajout des infos dans la fonction deposer
+
+            //Retrait sur le compte
+
+                //Ajout des infos dans la fonction retirer
+
+        //4 Affichage de la liste des clients
+
+        //5 Affichage de la liste des comptes
+
+        //6 Sauvegarde des données
+
+    return 0;
+}
